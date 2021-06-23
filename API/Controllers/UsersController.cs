e01760905a06160a4f1d2ec74b6f2bf705990611
Ioannis Karamitsos <karamitsos.i@sga.gr>
2021-06-23 17:36:01 +0300

    initialize front login

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -23,8 +23,7 @@ namespace API.Controllers
         [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() 
         {
-            return await _context.Users.ToListAsync();
-            
+            return await _context.Users.ToListAsync(); 
         }
 
         //    api/users/3
---
+++ Web/src/app/services/account.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  base_url = 'https://localhost:5001/api/';
+
+  constructor(private http: HttpClient) { }
+
+  login(model: any) {
+    return this.http.post(this.base_url + 'Account/login', model)
+  }
+}
