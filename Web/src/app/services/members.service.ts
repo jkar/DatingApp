7f6d3da6362370ade8701cf5800dae85cc8a53fd
Ioannis Karamitsos <karamitsos.i@sga.gr>
2021-06-30 00:08:27 +0300

    ui members-list

--- Web/src/app/services/members.service.ts
+++ Web/src/app/services/members.service.ts
@@ -4,21 +4,32 @@ import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Member } from '../models/member';
 
-const httpOptions = {
-  headers : new HttpHeaders ({
-    Authorization: 'Bearer ' + JSON.parse(localStorage.getItem("user")).token
-  })
-}
+// const httpOptions = {
+//   headers : new HttpHeaders ({
+//     Authorization:  'Bearer ' + JSON.parse(localStorage.getItem('user'))?.token
+//     })
+// }
 
 @Injectable({
   providedIn: 'root'
 })
 export class MembersService {
   baseUrl = environment.base_url;
+  httpOptions = {};
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.httpOptions = {
+      headers : new HttpHeaders ({
+        Authorization:  'Bearer ' + JSON.parse(localStorage.getItem('user'))?.token
+        })
+    }
+   }
 
   getMembers(): Observable<Member[]> {
-    return this.http.get<Member[]>(this.baseUrl + 'Users', httpOptions)
+    return this.http.get<Member[]>(this.baseUrl + 'Users', this.httpOptions)
+  }
+
+  getMember(username: string) {
+    return this.http.get<Member>(this.baseUrl + '/Users' + username, this.httpOptions);
   }
 }
