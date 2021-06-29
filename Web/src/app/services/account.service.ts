ce0c19dc146ef83f09361da2475b866fb4fd5067
Ioannis Karamitsos <karamitsos.i@sga.gr>
2021-06-29 14:55:41 +0300

    user interface start

--- Web/src/app/services/account.service.ts
+++ Web/src/app/services/account.service.ts
@@ -2,13 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ReplaySubject } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-  base_url = 'https://localhost:5001/api/';
+  base_url = environment.base_url;
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
 
---
+++ Web/src/app/services/members.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Member } from '../models/member';
+
+const httpOptions = {
+  headers : new HttpHeaders ({
+    Authorization: 'Bearer ' + JSON.parse(localStorage.getItem("user")).token
+  })
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MembersService {
+  baseUrl = environment.base_url;
+
+  constructor(private http: HttpClient) { }
+
+  getMembers(): Observable<Member[]> {
+    return this.http.get<Member[]>(this.baseUrl + 'Users', httpOptions)
+  }
+}
