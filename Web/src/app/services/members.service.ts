110f8bf11319a9c0b29b6c5ff07d8fcde38a2ed3
Ioannis Karamitsos <karamitsos.i@sga.gr>
2021-08-26 17:02:16 +0300

    deleting photo front

--- Web/src/app/services/members.service.ts
+++ Web/src/app/services/members.service.ts
@@ -58,4 +58,8 @@ export class MembersService {
   setMainPhoto (photoId: number) {
     return this.http.put(this.baseUrl + 'Users/set-main-photo/' + photoId, {});
   }
+
+  deletePhoto (photoId: number) {
+    return this.http.delete(this.baseUrl + 'Users/delete-photo/' + photoId);
+  }
 }
