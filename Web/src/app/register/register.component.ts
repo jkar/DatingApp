33ba5aa8d0289b2ad0661401cc85f80edeb556e8
Ioannis Karamitsos <karamitsos.i@sga.gr>
2021-08-29 20:30:23 +0300

    update front register method

--- Web/src/app/register/register.component.ts
+++ Web/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../services/account.service';
 
@@ -11,14 +12,16 @@ import { AccountService } from '../services/account.service';
 export class RegisterComponent implements OnInit {
   @Input() usersFromHomeComponent: any;
   @Output() cancelRegister = new EventEmitter<boolean>();
-  model: { username: string; password: string; } = { username: '', password: ''};
+  //model: { username: string; password: string; } = { username: '', password: ''};
   registerForm: FormGroup;
   maxDate: Date;
+  validationErrors: string[] = [];
 
   constructor(
               private accountService: AccountService,
               private toastr: ToastrService,
-              private fb: FormBuilder
+              private fb: FormBuilder,
+              private router: Router
               ) { }
 
   ngOnInit(): void {
@@ -57,17 +60,17 @@ export class RegisterComponent implements OnInit {
   }
 
   register() {
-    console.log(this.registerForm.value);
-    // this.accountService.register(this.model)
-    //   .subscribe(response => {
-    //     console.log(response);
-    //     this.cancel();
-    //   },
-    //   error => {
-    //     console.log(error);
-    //     this.toastr.error(error.error);
-    //   }
-    //   );
+    this.accountService.register(this.registerForm.value)
+      .subscribe(response => {
+        console.log(response);
+        this.router.navigateByUrl('/members');
+      },
+      error => {
+        // console.log(error);
+        // this.toastr.error(error.error);
+        this.validationErrors = error;
+      }
+      );
   }
 
   cancel() {
