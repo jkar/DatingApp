7ccc8cf9669c6c08223974ee57db4f2bc82bf884
Ioannis Karamitsos <karamitsos.i@sga.gr>
2021-09-03 12:07:53 +0300

    send messages +messagesController

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -21,7 +21,13 @@ namespace API.Helpers
             CreateMap<Photo, PhotoDto>();
             CreateMap<MemberUpdateDTO, AppUser>();
             CreateMap<RegisterDto, AppUser>();
+
+            //kanw to mapping apo Message sto MessageDto kai epd ta SenderPhotoUrl, RecipientPhotoUrl den einai idia, ta kanw populate manually 
+            CreateMap<Message, MessageDto>()
+                .ForMember(dest => dest.SenderPhotoUrl, opt => opt.MapFrom(src => 
+                    src.Sender.Photos.FirstOrDefault(x => x.IsMain).Url))
+                .ForMember(dest => dest.RecipientPhotoUrl, opt => opt.MapFrom(src => 
+                    src.Recipient.Photos.FirstOrDefault(x => x.IsMain).Url));
         }
-        
     }
 }
